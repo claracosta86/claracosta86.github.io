08d9412e38ccf510780596bb5991ea592e656e16
claracosta86 <clara004.costa@gmail.com>
2025-07-21 18:22:05 -0300

    fix: cors

--- back/main.go
+++ back/main.go
@@ -57,10 +57,15 @@ func tratarEvento(w http.ResponseWriter, r *http.Request) {
 
 func enableCors(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "https://seu-usuario.github.io")
+        origin := r.Header.Get("Origin")
+        if origin == "http://127.0.0.1:5500" || origin == "https://seu-usuario.github.io" {
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+        }
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
         if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
             return
         }
         next.ServeHTTP(w, r)
